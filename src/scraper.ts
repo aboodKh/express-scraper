7e6a6a5fcfaf3b8c24a56a665fe1d643b01fcf3c
Abdullah Khallouf <a.khallouf@outlook.com>
2021-07-09 21:40:49 +0300

    added browser environment to .eslintrc.json to ignore no-undef rule related to broser global variables such as document

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-	const browser = await puppeteer.launch({headless: false});
+	const browser = await puppeteer.launch({headless: true});
 	const page = await browser.newPage();
 	await page.goto("https://majra.me/opportunities");
 
@@ -21,7 +21,7 @@ import puppeteer from "puppeteer";
 	await page.evaluate(() => {
 		// redundant step, but show how we get to remote anchor tag
 		$("button[title = 'All Types' ]").click();
-		// eslint-disable-next-line no-undef
+
 		const a: HTMLAnchorElement | null = document.querySelector(
 			"ul > li:nth-child(7) > a"
 		);
@@ -38,5 +38,5 @@ import puppeteer from "puppeteer";
 		return tits;
 	});
 	console.log(titles);
-	// await browser.close();
+	await browser.close();
 })();
