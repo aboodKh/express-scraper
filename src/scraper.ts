7b7e11acae695501c96ee1345844d0dbda7345ce
Abdullah Khallouf <a.khallouf@outlook.com>
2021-07-09 21:43:19 +0300

    removed commented code related to page.click because didn't work

--- src/scraper.ts
+++ src/scraper.ts
@@ -5,19 +5,6 @@ import puppeteer from "puppeteer";
 	const page = await browser.newPage();
 	await page.goto("https://majra.me/opportunities");
 
-	// try {
-	// 	await page.click("button[title = 'All Types']");
-	// } catch (e) {
-	// 	console.log("type button", e);
-	// }
-
-	// try {
-	// 	// await page.waitForSelector("ul > li:nth-child(7) > a", {timeout: 10000});
-	// 	await page.click("ul > li:nth-child(7)> a");
-	// } catch (e) {
-	// 	console.log("remote link", e);
-	// }
-
 	await page.evaluate(() => {
 		// redundant step, but show how we get to remote anchor tag
 		$("button[title = 'All Types' ]").click();
